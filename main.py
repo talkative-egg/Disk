d0fe8e3c2b7891f3529122a7951faf34721bd892
Peter Chen <chenjiajun0825@gmail.com>
2021-11-29 21:16:17 -0500

    Added credit

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+
+# The cmu_112_graphics file is from https://www.cs.cmu.edu/~112/notes/notes-graphics.html and credited to
+# Carnegie Mellon University
 from cmu_112_graphics import *
 import random
 
 
 def step(app):
     new_center = app.center
-    shortest_length = compute_longest(app, app.center)
+    shortest_length, furthest = compute_longest(app, app.center)
     for dx in [-3, 0, 3]:
         for dy in [-3, 0, 3]:
             if dx == dy == 0:
                 continue
             center = (app.center[0] + dx, app.center[1] + dy)
-            length = compute_longest(app, center)
+            length, furthest = compute_longest(app, center)
             if length < shortest_length:
                 shortest_length = length
                 new_center = center
@@ -19,6 +22,7 @@ def step(app):
         app.over = True
     else:
         app.longest_len = shortest_length
+        app.furthest_point = furthest
         app.center = new_center
 
 
@@ -35,13 +39,15 @@ def distance(x0, y0, x1, y1):
 
 def compute_longest(app, center):
     longest = 0
+    furthest = app.points[0]
 
     for point in app.points:
         dist = distance(point[0], point[1], center[0], center[1])
         if dist > longest:
             longest = dist
+            furthest = point
 
-    return longest
+    return longest, furthest
 
 
 def timerFired(app):
@@ -64,7 +70,7 @@ def appStarted(app):
     app.points = []
     reset_points(app, 10)
     app.center = (500, 400)
-    app.longest_len = compute_longest(app, app.center)
+    app.longest_len, app.furthest_point = compute_longest(app, app.center)
     app.over = False
     app.timer = 0
 
@@ -75,10 +81,13 @@ def redrawAll(app, canvas):
                        fill="blue")
 
     canvas.create_oval(app.center[0] - 4, app.center[1] - 4,
-                       app.center[0] + 4, app.center[1] + 4, fill="orange")
+                       app.center[0] + 4, app.center[1] + 4, fill="orange", width=0)
 
     for point in app.points:
-        canvas.create_oval(point[0] - 4, point[1] - 4, point[0] + 4, point[1] + 4, fill="black")
+        canvas.create_oval(point[0] - 4, point[1] - 4, point[0] + 4, point[1] + 4, fill="black", width=0)
+
+    canvas.create_line(app.furthest_point[0], app.furthest_point[1],
+                       app.center[0], app.center[1])
 
 
 if __name__ == '__main__':
